ead2fbb1d99fb7cf5e37734c6b6a59139e3acb04
saideeWD <mdsaideehasan@gmail.com>
2024-11-17 16:56:29 +0600

    projects start

--- app/ui/Main.js
+++ app/ui/Main.js
@@ -4,7 +4,7 @@ export default function Main({children}) {
   return (
     <div className='text-gray-500 bg-gray-100 p-4 sm:ml-64 flex gap-2 flex-col lg:flex-row translate-all duration-300 mt-14 dark:bg-gray-800 p-4 sm:ml-64 '>
       {
-        children
+        childre
       }
     
     </div>
